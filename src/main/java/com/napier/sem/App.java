945f1d31f49106bcc6888b93b8a5ee3c23439dab
kendailherbertedu-art <kendailherbert.edu@gmail.com>
2025-10-11 18:43:40 -0600

    Added Hello World

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -11,7 +11,8 @@ public class App
     public static void main(String[] args)
     {
         // Connect to MongoDB on local system - we're using port 27000
-        MongoClient mongoClient = MongoClients.create("mongodb://localhost:27000");
+        // Connect to MongoDB
+        MongoClient mongoClient = MongoClients.create("mongodb://mongo-dbserver:27017");
         // Get a database - will create when we use it
         MongoDatabase database = mongoClient.getDatabase("mydb");
         // Get a collection from the database
